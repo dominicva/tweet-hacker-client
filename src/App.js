0909aa91c8439f4c81cb94655e0f9171e428a3d9
Dominic <dominicvana@gmail.com>
2022-05-08 23:11:58 -0500

    Feat: rendering tweets on form submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState, useEffect, Fragment } from 'react';
 import './App.css';
 
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await fetch(url).then(r => r.json());
+        setData(result);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    getData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+};
+
 function App() {
+  const [query, setQuery] = useState('dominicva');
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    `http://localhost:3001/api/tweets/${query}`,
+    {
+      data: {
+        user: {},
+        recent_tweets: [],
+      },
+    }
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Fragment>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            doFetch(`http://localhost:3001/api/tweets/${query}`);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          ></input>
+          <button type="submit">Search</button>
+        </form>
+
+        {isError && <div>Something went wrong...</div>}
+
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {data.data.recent_tweets.map(tweet => (
+              <li key={tweet.id}>
+                <p>{tweet.text}</p>
+              </li>
+            ))}
+          </div>
+        )}
+      </Fragment>
     </div>
   );
 }
